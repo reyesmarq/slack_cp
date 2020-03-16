f5510d1400762161d6f5830afae53d281a994760
Mau Reyes <mauricio.reyes@rsmus.com>
2020-03-15 20:18:10 -0600

    handling register form errors

--- server/resolvers/user.js
+++ server/resolvers/user.js
@@ -1,4 +1,13 @@
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcrypt';
+import _ from 'lodash';
+
+const formatErrors = (e, models) => {
+    if (e instanceof models.sequelize.ValidationError) {
+        //  _.pick({a: 1, b: 2}, 'a') => {a: 1}
+        return e.errors.map(x => _.pick(x, ['path', 'message']));
+    }
+    return [{ path: 'name', message: 'something went wrong' }];
+};
 
 export default {
     Query: {
@@ -8,11 +17,30 @@ export default {
     Mutation: {
         register: async (parent, { password, ...otherArgs }, { models }) => {
             try {
-                let hashedPassword = await bcrypt.hash(password, 12)
-                await models.User.create({ ...otherArgs, password: hashedPassword })
-                return true
-            } catch (error) {
-                return false
+                if (password.length < 5 || password.length > 100) {
+                    return {
+                        ok: false,
+                        errors: [
+                            {
+                                path: 'password',
+                                message: 'The password needs to be between 5 and 100 characters long',
+                            },
+                        ],
+                    };
+                }
+
+                const hashedPassword = await bcrypt.hash(password, 12);
+                const user = await models.User.create({ ...otherArgs, password: hashedPassword });
+
+                return {
+                    ok: true,
+                    user,
+                };
+            } catch (err) {
+                return {
+                    ok: false,
+                    errors: formatErrors(err, models),
+                };
             }
         },
     },
--- server/schema/user.js
+++ server/schema/user.js
@@ -1,17 +1,22 @@
 export default `
     type User {
         id: Int!
-        email: String!
         username: String!
+        email: String!
         teams: [Team!]!
     }
-
+    
     type Query {
         getUser(id: Int!): User!
         allUsers: [User!]!
     }
+    type RegisterResponse {
+        ok: Boolean!
+        user: User
+        errors: [Error!]
+    }
 
     type Mutation {
-        register(username: String!, email: String!, password: String!): Boolean!
+        register(username: String!, email: String!, password: String!): RegisterResponse!
     }
 `
