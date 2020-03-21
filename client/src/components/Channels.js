5674e4035980745cd5e29b7b61a1f120d6af821c
Mau Reyes <mauricio.reyes@rsmus.com>
2020-03-21 00:25:57 -0600

    Building slack sidebar with styled components

--- client/src/components/Channels.js
+++ client/src/components/Channels.js
@@ -1,34 +1,69 @@
-import React from 'react'
+import React from 'react';
 import styled from 'styled-components';
 
 const ChannelWrapper = styled.div`
   grid-column: 2;
   grid-row: 1 / 4;
   background-color: #4e3a4c;
-  color: #958993
+  color: #958993;
 `;
 
-const channel = ({ id, name }) => <li key={`channel-${id}`}># {name}</li>
+const TeamNameHeader = styled.h1`
+  color: #fff;
+  font-size: 20px;
+`;
 
-const user = ({ id, name }) => <li key={`user-${id}`}>{name}</li>
+const SideBarList = styled.ul`
+  width: 100%;
+  list-style: none;
+  padding-left: 0px;
+`;
 
-const Channels = ({ teamName, username, channels, users }) => (
-    <ChannelWrapper>
-        <div>
-            {teamName}
-            {username}
-        </div>
-        <div>
-            <ul>
-                <li>Channels</li>
-                {channels.map(channel)}
-            </ul>
-            <ul>
-                <li>USers</li>
-                {users.map(user)}
-            </ul>
-        </div>
-    </ChannelWrapper>
-)
+const paddingLeft = 'padding-left: 10px';
+
+const SideBarListItem = styled.li`
+  padding: 2px;
+  ${paddingLeft};
+  &:hover {
+    background: #3e313c;
+  }
+`;
+
+const SideBarListHeader = styled.li`${paddingLeft};`;
+
+const PushLeft = styled.div`${paddingLeft};`;
+
+const Green = styled.span`color: #38978d;`;
+
+const Bubble = ({ on = true }) => (on ? <Green>●</Green> : '○');
+
+const channel = ({ id, name }) => <SideBarListItem key={`channel-${id}`}># {name}</SideBarListItem>;
+
+const user = ({ id, name }) => (
+  <SideBarListItem key={`user-${id}`}>
+    <Bubble /> {name}
+  </SideBarListItem>
+);
+
+const Channels = ({ teamName, username, channels, users, }) => (
+  <ChannelWrapper>
+    <PushLeft>
+      <TeamNameHeader>{teamName}</TeamNameHeader>
+      {username}
+    </PushLeft>
+    <div>
+      <SideBarList>
+        <SideBarListHeader>Channels</SideBarListHeader>
+        {channels.map(channel)}
+      </SideBarList>
+    </div>
+    <div>
+      <SideBarList>
+        <SideBarListHeader>Direct Messages</SideBarListHeader>
+        {users.map(user)}
+      </SideBarList>
+    </div>
+  </ChannelWrapper>
+);
 
 export default Channels
