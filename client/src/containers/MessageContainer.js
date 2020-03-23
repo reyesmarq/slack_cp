427bfe7595139ad5954ceaa5be9148b21e3d35a0
Mau Reyes <mauricio.reyes@rsmus.com>
2020-03-22 22:01:40 -0600

    adding messages to channel - semantic ui react comment component

--- client/src/containers/MessageContainer.js
+++ client/src/containers/MessageContainer.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
+import { Comment } from 'semantic-ui-react'
 
 import Messages from '../components/Messages';
 
 const MessageContainer = ({ data: { loading, messages } }) =>
-  (loading ? null : <Messages>{JSON.stringify(messages)}</Messages>);
+  (loading ? null : <Messages>
+    <Comment.Group>
+      {messages.map(m => (
+        <Comment key={`${m.id}-message`}>
+          <Comment.Content>
+            <Comment.Author as="a">{m.user.username}</Comment.Author>
+            <Comment.Metadata>
+              <div>{new Date(parseInt(m.createdAt)).toDateString()}</div>
+            </Comment.Metadata>
+            <Comment.Text>{m.text}</Comment.Text>
+            <Comment.Actions>
+              <Comment.Action>Reply</Comment.Action>
+            </Comment.Actions>
+          </Comment.Content>
+        </Comment>
+      ))}
+    </Comment.Group>
+  </Messages>);
 
 const messagesQuery = gql`
   query($channelId: Int!) {
--- server/resolvers/message.js
+++ server/resolvers/message.js
@@ -1,9 +1,15 @@
+import requiresAuth from '../permissions';
+
 export default {
+  Message: {
+    // if of our message
+    user: ({ userId }, args, { models }) => models.User.findOne({ where: { id: userId } }),
+  },
   Query: {
-    messages: async (parent, args, { models, user }) => [],
+    messages: requiresAuth.createResolver(async (parent, { channelId }, { models, user }) => models.Message.findAll({ order: [['createdAt', 'ASC']], where: { channelId } }, { raw: true })),
   },
   Mutation: {
-    createMessage: async (parent, args, { models, user }) => {
+    createMessage: requiresAuth.createResolver(async (parent, args, { models, user }) => {
       try {
         await models.Message.create({
           ...args,
@@ -14,6 +20,6 @@ export default {
         console.log(err);
         return false;
       }
-    },
+    }),
   },
 };
