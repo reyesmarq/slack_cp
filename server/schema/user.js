d9f68a6e4bf845d06f0bfbff6adc08c12f09b75d
Mau Reyes <mauricio.reyes@rsmus.com>
2020-04-01 19:57:18 -0600

    updating cache after a graphql mutation

--- server/schema/user.js
+++ server/schema/user.js
@@ -1,34 +1,28 @@
 export default `
-
   type User {
     id: Int!
     username: String!
     email: String!
     teams: [Team!]!
   }
-
   type Query {
     me: User!
     allUsers: [User!]!
     getUser(userId: Int!): User
   }
-
   type RegisterResponse {
     ok: Boolean!
     user: User
     errors: [Error!]
   }
-
   type LoginResponse {
     ok: Boolean!
     token: String
     refreshToken: String
     errors: [Error!]
   }
-
   type Mutation {
     register(username: String!, email: String!, password: String!): RegisterResponse!
     login(email: String!, password: String!): LoginResponse!
   }
-
 `;
